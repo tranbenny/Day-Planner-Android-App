395ae874946159e4873b4620a9d039d315c1624a
tranbenny <tran.benny123@gmail.com>
2016-02-12 12:32:40 -0800

    first commit

---
+++ app/src/main/java/com/example/bennytran/yelpagendabuilder/PlanActivity.java
@@ -0,0 +1,21 @@
+package com.example.bennytran.yelpagendabuilder;
+
+// this activity will hold the results for a user's plan
+// plan will be based on user preferences, location, and time interval options
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class PlanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plan);
+
+        // create a fragment here to hold the information
+
+    }
+}
